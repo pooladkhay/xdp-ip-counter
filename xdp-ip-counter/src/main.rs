b7d92e2f006260d7740daddbfd288fc78560e586
Mohammad Javad Pooladkhay <m.pooladkhay@gmail.com>
2023-04-18 14:34:35 -0400

    scoped local variables for spawn functions

--- xdp-ip-counter/src/main.rs
+++ xdp-ip-counter/src/main.rs
@@ -21,19 +21,19 @@ async fn main() -> Result<(), anyhow::Error> {
     let mut shared_maps = structs::SharedMaps::new(&ebpf);
     let local_maps = Arc::new(Mutex::new(structs::LocalMaps::new()));
 
-    let lm1 = local_maps.clone();
-    let aggregate_window = args.parse_window();
-    tokio::spawn(
-        async move { ebpf::generate_metrics(&mut shared_maps, lm1, aggregate_window).await },
-    );
+    tokio::spawn({
+        let local_maps = local_maps.clone();
+        let aggregate_window = args.parse_window();
+        async move { ebpf::generate_metrics(&mut shared_maps, local_maps, aggregate_window).await }
+    });
 
-    let lm2 = local_maps.clone();
-    let custom_ports = args.parse_custom_ports();
-    let server_port = args.parse_server_port();
-    let serve_ip_list = args.serve_ip_list;
-    tokio::spawn(
-        async move { api::server::serve(lm2, custom_ports, server_port, serve_ip_list).await },
-    );
+    tokio::spawn({
+        let local_maps = local_maps.clone();
+        let custom_ports = args.parse_custom_ports();
+        let server_port = args.parse_server_port();
+        let serve_ip_list = args.serve_ip_list;
+        async move { api::server::serve(local_maps, custom_ports, server_port, serve_ip_list).await }
+    });
 
     info!("Waiting for Ctrl-C...");
     signal::ctrl_c().await?;
